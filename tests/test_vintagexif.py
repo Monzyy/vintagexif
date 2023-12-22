d82dfb51bf514ff868251aabdf5dfcb7bf510574
Magnus Hansen <magnushansen12@hotmail.com>
2023-12-22 15:39:35 +0100

    Solve year and month dir

--- tests/test_vintagexif.py
+++ tests/test_vintagexif.py
@@ -9,11 +9,6 @@ FIXTURE_DIR = Path(__file__).parent / "fixtures"
 
 
 @pytest.fixture()
-def only_year_dir() -> Path:
-    return FIXTURE_DIR / "only_year_dir"
-
-
-@pytest.fixture()
 def empty_dir() -> Path:
     return FIXTURE_DIR / "empty_dir"
 
@@ -22,6 +17,19 @@ def test_empty_dir_returns_empty_map(empty_dir):
     assert get_image_date_mapping(source_dir=empty_dir) == {}
 
 
+@pytest.fixture()
+def only_year_dir() -> Path:
+    return FIXTURE_DIR / "only_year_dir"
+
+
 def test_should_get_image_date_map_for_dir_with_year(only_year_dir):
     assert get_image_date_mapping(only_year_dir) == {only_year_dir / "1995" / "vintage.jpg": datetime(year=1995, month=1, day=1)}
 
+
+@pytest.fixture()
+def year_month_dir() -> Path:
+    return FIXTURE_DIR / "year_month_dir"
+
+
+def test_should_get_image_date_map_for_year_month_dir(year_month_dir):
+    assert get_image_date_mapping(year_month_dir) == {year_month_dir / "1994" / "02" / "vintage.jpg": datetime(year=1994, month=2, day=1)}
--- vintagexif.py
+++ vintagexif.py
@@ -1,24 +1,38 @@
 import os
 from datetime import datetime
 from pathlib import Path
-from typing import Mapping
+from typing import Mapping, Optional
 
 
 def get_image_date_mapping(source_dir: Path) -> Mapping[Path, datetime]:
     map = {}
 
     for root, dirs, files in os.walk(source_dir):
+        root = Path(root)
         year, month, day = None, 1, 1
         if not len(files):
             continue
 
-        try:
-            year = int(1995)
-        except ValueError:
+        root_value = try_parse_date(root.name)
+        if root_value is None:
             continue
 
+        parent_value = try_parse_date(root.parent.name)
+        if parent_value is not None:
+            year = parent_value
+            month = root_value
+        else:
+            year = root_value
+
         for file in files:
-            map[Path(f"{root}/{file}")] = datetime(year=year, month=month, day=day)
+            map[root / file] = datetime(year=year, month=month, day=day)
 
     return map
 
+
+def try_parse_date(date: str) -> Optional[int]:
+    try:
+        value = int(date)
+    except ValueError:
+        return None
+    return value
